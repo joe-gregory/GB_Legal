f3d305d82b435442d85f33908c1884fd448469dd
joe-gregory <josephedward2nd@gmail.com>
2023-06-07 09:20:43 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GB_Legal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
